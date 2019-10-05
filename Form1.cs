257f925b4c5400dc602e35d9ef1ef1dfeeaa0b7c
Ricardo Pérez <ricardope1664@gmail.com>
2019-10-05 16:05:12 -0600

    PY1_ORGA

---
+++ Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cerrarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void abrirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            /*Abrir archivos .draw*/
+
+            /* Crear pestaña con la informacion obtenida.*/
+            TabPage nuevoPage = new TabPage("1.draw");
+            tabControl1.Controls.Add(nuevoPage);
+        }
+    }
+}
